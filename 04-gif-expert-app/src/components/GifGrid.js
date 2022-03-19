187dea02fd8e1f7e4973d0602cf22fdf00c816d3
George <georgegaibor@gmail.com>
2022-03-19 15:21:35 -0500

    Finished Section 6

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,20 +1,16 @@
-import React, {useState, useEffect} from 'react'
-import { getGifs } from '../helpers/getGifs';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGIfs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ( { category } ) => {
     
-    const [images, setImages] = useState([]);
-
-    useEffect(() => {
-      getGifs(`${category}`)
-        .then(gifs => setImages(gifs));
-    }, [ category ])
-    
+    const {data:images, loading} = useFetchGifs(category);
+        
     
     return (
         <>
-            <h3>{category}</h3>
+            <h3 className='animate__animated animate__fadeIn'>{category}</h3>
+            {loading && <p className='animate__animated animate__flash'>Loading</p>}
             <div className='card-grid'>
                 
                 {
